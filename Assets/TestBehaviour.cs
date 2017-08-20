3db67a67a8fcce181a13fe3c10ddf6557b96fbf9
KonH <konh@yandex.ru>
2017-08-20 14:01:23 +0700

    Test project init;

---
+++ Assets/TestBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TestBehaviour : MonoBehaviour {
+
+	TestClass _test;
+
+	void Start () {
+		InstanceMethod();
+	}
+
+	void InstanceMethod() {
+		TestClass.StaticMethod();
+		_test = new TestClass(0);
+	}
+
+	void Update () {
+		_test.InstanceMethod();
+	}
+}
---
+++ Assets/TestClass.cs
@@ -0,0 +1,15 @@
+﻿public class TestClass {
+
+	int _a;
+
+	public TestClass(int a) {
+		_a = a;
+	}
+
+	public static void StaticMethod() {
+	}
+
+	public void InstanceMethod() {
+		_a++;
+	}
+}
