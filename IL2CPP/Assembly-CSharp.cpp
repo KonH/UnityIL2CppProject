6779171c9dc206a1862ed76f432eeeacff4b79d9
KonH <konh@yandex.ru>
2018-05-03 22:18:25 +0700

    Updated script && samples;

--- IL2CPP/Assembly-CSharp.cpp
+++ IL2CPP/Assembly-CSharp.cpp
@@ -12,115 +12,473 @@
 #include <cmath>
 #include <limits>
 #include <assert.h>
+#include <stdint.h>
 
-#include "class-internals.h"
+#include "il2cpp-class-internals.h"
 #include "codegen/il2cpp-codegen.h"
-#include "mscorlib_System_Array3829468939.h"
-#include "AssemblyU2DCSharp_U3CModuleU3E3783534214.h"
-#include "AssemblyU2DCSharp_TestBehaviour930884329.h"
-#include "mscorlib_System_Void1841601450.h"
-#include "UnityEngine_UnityEngine_MonoBehaviour1158329972.h"
-#include "AssemblyU2DCSharp_TestClass1183229268.h"
-#include "mscorlib_System_Int322071877448.h"
-#include "mscorlib_System_Object2689449295.h"
+#include "il2cpp-object-internals.h"
+
 
 // TestBehaviour
-struct TestBehaviour_t930884329;
+struct TestBehaviour_t1573233452;
 // UnityEngine.MonoBehaviour
-struct MonoBehaviour_t1158329972;
+struct MonoBehaviour_t3962482529;
 // TestClass
-struct TestClass_t1183229268;
-// System.Object
-struct Il2CppObject;
-extern Il2CppClass* TestClass_t1183229268_il2cpp_TypeInfo_var;
-extern const uint32_t TestBehaviour_InstanceMethod_m3802251226_MetadataUsageId;
+struct TestClass_t492893797;
+// System.Void
+struct Void_t1185182177;
 
+extern RuntimeClass* TestClass_t492893797_il2cpp_TypeInfo_var;
+extern const uint32_t TestBehaviour_InstanceMethod_m1909480579_MetadataUsageId;
+
+
+
+#ifndef U3CMODULEU3E_T692745535_H
+#define U3CMODULEU3E_T692745535_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// <Module>
+struct  U3CModuleU3E_t692745535 
+{
+public:
+
+public:
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // U3CMODULEU3E_T692745535_H
+#ifndef RUNTIMEOBJECT_H
+#define RUNTIMEOBJECT_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// System.Object
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // RUNTIMEOBJECT_H
+struct Il2CppArrayBounds;
+#ifndef RUNTIMEARRAY_H
+#define RUNTIMEARRAY_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// System.Array
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // RUNTIMEARRAY_H
+#ifndef TESTCLASS_T492893797_H
+#define TESTCLASS_T492893797_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// TestClass
+struct  TestClass_t492893797  : public RuntimeObject
+{
+public:
+	// System.Int32 TestClass::_a
+	int32_t ____a_0;
+
+public:
+	inline static int32_t get_offset_of__a_0() { return static_cast<int32_t>(offsetof(TestClass_t492893797, ____a_0)); }
+	inline int32_t get__a_0() const { return ____a_0; }
+	inline int32_t* get_address_of__a_0() { return &____a_0; }
+	inline void set__a_0(int32_t value)
+	{
+		____a_0 = value;
+	}
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // TESTCLASS_T492893797_H
+#ifndef VALUETYPE_T3640485471_H
+#define VALUETYPE_T3640485471_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// System.ValueType
+struct  ValueType_t3640485471  : public RuntimeObject
+{
+public:
+
+public:
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+// Native definition for P/Invoke marshalling of System.ValueType
+struct ValueType_t3640485471_marshaled_pinvoke
+{
+};
+// Native definition for COM marshalling of System.ValueType
+struct ValueType_t3640485471_marshaled_com
+{
+};
+#endif // VALUETYPE_T3640485471_H
+#ifndef VOID_T1185182177_H
+#define VOID_T1185182177_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// System.Void
+struct  Void_t1185182177 
+{
+public:
+
+public:
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // VOID_T1185182177_H
+#ifndef INT32_T2950945753_H
+#define INT32_T2950945753_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// System.Int32
+struct  Int32_t2950945753 
+{
+public:
+	// System.Int32 System.Int32::m_value
+	int32_t ___m_value_2;
+
+public:
+	inline static int32_t get_offset_of_m_value_2() { return static_cast<int32_t>(offsetof(Int32_t2950945753, ___m_value_2)); }
+	inline int32_t get_m_value_2() const { return ___m_value_2; }
+	inline int32_t* get_address_of_m_value_2() { return &___m_value_2; }
+	inline void set_m_value_2(int32_t value)
+	{
+		___m_value_2 = value;
+	}
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // INT32_T2950945753_H
+#ifndef INTPTR_T_H
+#define INTPTR_T_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// System.IntPtr
+struct  IntPtr_t 
+{
+public:
+	// System.Void* System.IntPtr::m_value
+	void* ___m_value_0;
+
+public:
+	inline static int32_t get_offset_of_m_value_0() { return static_cast<int32_t>(offsetof(IntPtr_t, ___m_value_0)); }
+	inline void* get_m_value_0() const { return ___m_value_0; }
+	inline void** get_address_of_m_value_0() { return &___m_value_0; }
+	inline void set_m_value_0(void* value)
+	{
+		___m_value_0 = value;
+	}
+};
+
+struct IntPtr_t_StaticFields
+{
+public:
+	// System.IntPtr System.IntPtr::Zero
+	intptr_t ___Zero_1;
+
+public:
+	inline static int32_t get_offset_of_Zero_1() { return static_cast<int32_t>(offsetof(IntPtr_t_StaticFields, ___Zero_1)); }
+	inline intptr_t get_Zero_1() const { return ___Zero_1; }
+	inline intptr_t* get_address_of_Zero_1() { return &___Zero_1; }
+	inline void set_Zero_1(intptr_t value)
+	{
+		___Zero_1 = value;
+	}
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // INTPTR_T_H
+#ifndef OBJECT_T631007953_H
+#define OBJECT_T631007953_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// UnityEngine.Object
+struct  Object_t631007953  : public RuntimeObject
+{
+public:
+	// System.IntPtr UnityEngine.Object::m_CachedPtr
+	intptr_t ___m_CachedPtr_0;
+
+public:
+	inline static int32_t get_offset_of_m_CachedPtr_0() { return static_cast<int32_t>(offsetof(Object_t631007953, ___m_CachedPtr_0)); }
+	inline intptr_t get_m_CachedPtr_0() const { return ___m_CachedPtr_0; }
+	inline intptr_t* get_address_of_m_CachedPtr_0() { return &___m_CachedPtr_0; }
+	inline void set_m_CachedPtr_0(intptr_t value)
+	{
+		___m_CachedPtr_0 = value;
+	}
+};
+
+struct Object_t631007953_StaticFields
+{
+public:
+	// System.Int32 UnityEngine.Object::OffsetOfInstanceIDInCPlusPlusObject
+	int32_t ___OffsetOfInstanceIDInCPlusPlusObject_1;
+
+public:
+	inline static int32_t get_offset_of_OffsetOfInstanceIDInCPlusPlusObject_1() { return static_cast<int32_t>(offsetof(Object_t631007953_StaticFields, ___OffsetOfInstanceIDInCPlusPlusObject_1)); }
+	inline int32_t get_OffsetOfInstanceIDInCPlusPlusObject_1() const { return ___OffsetOfInstanceIDInCPlusPlusObject_1; }
+	inline int32_t* get_address_of_OffsetOfInstanceIDInCPlusPlusObject_1() { return &___OffsetOfInstanceIDInCPlusPlusObject_1; }
+	inline void set_OffsetOfInstanceIDInCPlusPlusObject_1(int32_t value)
+	{
+		___OffsetOfInstanceIDInCPlusPlusObject_1 = value;
+	}
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+// Native definition for P/Invoke marshalling of UnityEngine.Object
+struct Object_t631007953_marshaled_pinvoke
+{
+	intptr_t ___m_CachedPtr_0;
+};
+// Native definition for COM marshalling of UnityEngine.Object
+struct Object_t631007953_marshaled_com
+{
+	intptr_t ___m_CachedPtr_0;
+};
+#endif // OBJECT_T631007953_H
+#ifndef COMPONENT_T1923634451_H
+#define COMPONENT_T1923634451_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// UnityEngine.Component
+struct  Component_t1923634451  : public Object_t631007953
+{
+public:
+
+public:
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // COMPONENT_T1923634451_H
+#ifndef BEHAVIOUR_T1437897464_H
+#define BEHAVIOUR_T1437897464_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// UnityEngine.Behaviour
+struct  Behaviour_t1437897464  : public Component_t1923634451
+{
+public:
+
+public:
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // BEHAVIOUR_T1437897464_H
+#ifndef MONOBEHAVIOUR_T3962482529_H
+#define MONOBEHAVIOUR_T3962482529_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// UnityEngine.MonoBehaviour
+struct  MonoBehaviour_t3962482529  : public Behaviour_t1437897464
+{
+public:
+
+public:
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // MONOBEHAVIOUR_T3962482529_H
+#ifndef TESTBEHAVIOUR_T1573233452_H
+#define TESTBEHAVIOUR_T1573233452_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// TestBehaviour
+struct  TestBehaviour_t1573233452  : public MonoBehaviour_t3962482529
+{
+public:
+	// TestClass TestBehaviour::_test
+	TestClass_t492893797 * ____test_2;
+
+public:
+	inline static int32_t get_offset_of__test_2() { return static_cast<int32_t>(offsetof(TestBehaviour_t1573233452, ____test_2)); }
+	inline TestClass_t492893797 * get__test_2() const { return ____test_2; }
+	inline TestClass_t492893797 ** get_address_of__test_2() { return &____test_2; }
+	inline void set__test_2(TestClass_t492893797 * value)
+	{
+		____test_2 = value;
+		Il2CppCodeGenWriteBarrier((&____test_2), value);
+	}
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // TESTBEHAVIOUR_T1573233452_H
 
 
 
 // System.Void UnityEngine.MonoBehaviour::.ctor()
-extern "C"  void MonoBehaviour__ctor_m2464341955 (MonoBehaviour_t1158329972 * __this, const MethodInfo* method) IL2CPP_METHOD_ATTR;
+extern "C"  void MonoBehaviour__ctor_m1579109191 (MonoBehaviour_t3962482529 * __this, const RuntimeMethod* method) IL2CPP_METHOD_ATTR;
 // System.Void TestBehaviour::InstanceMethod()
-extern "C"  void TestBehaviour_InstanceMethod_m3802251226 (TestBehaviour_t930884329 * __this, const MethodInfo* method) IL2CPP_METHOD_ATTR;
+extern "C"  void TestBehaviour_InstanceMethod_m1909480579 (TestBehaviour_t1573233452 * __this, const RuntimeMethod* method) IL2CPP_METHOD_ATTR;
 // System.Void TestClass::StaticMethod()
-extern "C"  void TestClass_StaticMethod_m3596601608 (Il2CppObject * __this /* static, unused */, const MethodInfo* method) IL2CPP_METHOD_ATTR;
+extern "C"  void TestClass_StaticMethod_m3291086251 (RuntimeObject * __this /* static, unused */, const RuntimeMethod* method) IL2CPP_METHOD_ATTR;
 // System.Void TestClass::.ctor(System.Int32)
-extern "C"  void TestClass__ctor_m3867534822 (TestClass_t1183229268 * __this, int32_t ___a0, const MethodInfo* method) IL2CPP_METHOD_ATTR;
+extern "C"  void TestClass__ctor_m3884429976 (TestClass_t492893797 * __this, int32_t ___a0, const RuntimeMethod* method) IL2CPP_METHOD_ATTR;
 // System.Void TestClass::InstanceMethod()
-extern "C"  void TestClass_InstanceMethod_m2173834289 (TestClass_t1183229268 * __this, const MethodInfo* method) IL2CPP_METHOD_ATTR;
+extern "C"  void TestClass_InstanceMethod_m1631011587 (TestClass_t492893797 * __this, const RuntimeMethod* method) IL2CPP_METHOD_ATTR;
 // System.Void System.Object::.ctor()
-extern "C"  void Object__ctor_m2551263788 (Il2CppObject * __this, const MethodInfo* method) IL2CPP_METHOD_ATTR;
+extern "C"  void Object__ctor_m297566312 (RuntimeObject * __this, const RuntimeMethod* method) IL2CPP_METHOD_ATTR;
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 #ifdef __clang__
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Winvalid-offsetof"
 #pragma clang diagnostic ignored "-Wunused-variable"
 #endif
 // System.Void TestBehaviour::.ctor()
-extern "C"  void TestBehaviour__ctor_m79303438 (TestBehaviour_t930884329 * __this, const MethodInfo* method)
+extern "C"  void TestBehaviour__ctor_m1233624187 (TestBehaviour_t1573233452 * __this, const RuntimeMethod* method)
 {
 	{
-		MonoBehaviour__ctor_m2464341955(__this, /*hidden argument*/NULL);
+		MonoBehaviour__ctor_m1579109191(__this, /*hidden argument*/NULL);
 		return;
 	}
 }
 // System.Void TestBehaviour::Start()
-extern "C"  void TestBehaviour_Start_m3566543662 (TestBehaviour_t930884329 * __this, const MethodInfo* method)
+extern "C"  void TestBehaviour_Start_m3318084427 (TestBehaviour_t1573233452 * __this, const RuntimeMethod* method)
 {
 	{
-		TestBehaviour_InstanceMethod_m3802251226(__this, /*hidden argument*/NULL);
+		TestBehaviour_InstanceMethod_m1909480579(__this, /*hidden argument*/NULL);
 		return;
 	}
 }
 // System.Void TestBehaviour::InstanceMethod()
-extern "C"  void TestBehaviour_InstanceMethod_m3802251226 (TestBehaviour_t930884329 * __this, const MethodInfo* method)
+extern "C"  void TestBehaviour_InstanceMethod_m1909480579 (TestBehaviour_t1573233452 * __this, const RuntimeMethod* method)
 {
 	static bool s_Il2CppMethodInitialized;
 	if (!s_Il2CppMethodInitialized)
 	{
-		il2cpp_codegen_initialize_method (TestBehaviour_InstanceMethod_m3802251226_MetadataUsageId);
+		il2cpp_codegen_initialize_method (TestBehaviour_InstanceMethod_m1909480579_MetadataUsageId);
 		s_Il2CppMethodInitialized = true;
 	}
 	{
-		TestClass_StaticMethod_m3596601608(NULL /*static, unused*/, /*hidden argument*/NULL);
-		TestClass_t1183229268 * L_0 = (TestClass_t1183229268 *)il2cpp_codegen_object_new(TestClass_t1183229268_il2cpp_TypeInfo_var);
-		TestClass__ctor_m3867534822(L_0, 0, /*hidden argument*/NULL);
+		TestClass_StaticMethod_m3291086251(NULL /*static, unused*/, /*hidden argument*/NULL);
+		TestClass_t492893797 * L_0 = (TestClass_t492893797 *)il2cpp_codegen_object_new(TestClass_t492893797_il2cpp_TypeInfo_var);
+		TestClass__ctor_m3884429976(L_0, 0, /*hidden argument*/NULL);
 		__this->set__test_2(L_0);
 		return;
 	}
 }
 // System.Void TestBehaviour::Update()
-extern "C"  void TestBehaviour_Update_m1664852881 (TestBehaviour_t930884329 * __this, const MethodInfo* method)
+extern "C"  void TestBehaviour_Update_m1067916034 (TestBehaviour_t1573233452 * __this, const RuntimeMethod* method)
 {
 	{
-		TestClass_t1183229268 * L_0 = __this->get__test_2();
-		NullCheck(L_0);
-		TestClass_InstanceMethod_m2173834289(L_0, /*hidden argument*/NULL);
+		TestClass_t492893797 * L_0 = __this->get__test_2();
+		TestClass_InstanceMethod_m1631011587(L_0, /*hidden argument*/NULL);
 		return;
 	}
 }
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
 // System.Void TestClass::.ctor(System.Int32)
-extern "C"  void TestClass__ctor_m3867534822 (TestClass_t1183229268 * __this, int32_t ___a0, const MethodInfo* method)
+extern "C"  void TestClass__ctor_m3884429976 (TestClass_t492893797 * __this, int32_t ___a0, const RuntimeMethod* method)
 {
 	{
-		Object__ctor_m2551263788(__this, /*hidden argument*/NULL);
+		Object__ctor_m297566312(__this, /*hidden argument*/NULL);
 		int32_t L_0 = ___a0;
 		__this->set__a_0(L_0);
 		return;
 	}
 }
 // System.Void TestClass::StaticMethod()
-extern "C"  void TestClass_StaticMethod_m3596601608 (Il2CppObject * __this /* static, unused */, const MethodInfo* method)
+extern "C"  void TestClass_StaticMethod_m3291086251 (RuntimeObject * __this /* static, unused */, const RuntimeMethod* method)
 {
 	{
 		return;
 	}
 }
 // System.Void TestClass::InstanceMethod()
-extern "C"  void TestClass_InstanceMethod_m2173834289 (TestClass_t1183229268 * __this, const MethodInfo* method)
+extern "C"  void TestClass_InstanceMethod_m1631011587 (TestClass_t492893797 * __this, const RuntimeMethod* method)
 {
 	{
 		int32_t L_0 = __this->get__a_0();
-		__this->set__a_0(((int32_t)((int32_t)L_0+(int32_t)1)));
+		__this->set__a_0(((int32_t)il2cpp_codegen_add((int32_t)L_0, (int32_t)1)));
 		return;
 	}
 }
